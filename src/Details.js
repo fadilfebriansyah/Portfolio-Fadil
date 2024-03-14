4214e638082187d018398f004f300a5d46c2cc4b
fadilfebriansyah <fadilfebriansyah76@gmail.com>
2024-03-14 20:11:45 +0700

    first commit

---
+++ src/Details.js
@@ -0,0 +1,208 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profilefdl.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+import php from "./assets/techstack/php.png";
+import python from "./assets/techstack/python.png";
+import mysql from "./assets/techstack/mysql.png";
+// Porject Images
+import projectImage1 from "./assets/projects/sayur.png";
+import projectImage2 from "./assets/projects/bengkel.png";
+import projectImage3 from "./assets/projects/online.png";
+import projectImage4 from "./assets/projects/klasifikasi.jpg";
+import projectImage5 from "./assets/projects/hujan.png";
+import projectImage6 from "./assets/projects/Taksi.png";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Fadil Febriansyah",
+  tagline: "I am a Data Scientist and Web Developer",
+  img: profile,
+  about: `Fadil Febriansyah, mahasiswi D-IV Informatika di Universitas Logistik dan Bisnis Internasional, berusia 21 tahun, memiliki keahlian dalam konfigurasi jaringan, pengembangan web (HTML dan CSS), serta pengembangan Rest API dengan Python. Memiliki ketertarikan khusus dalam ilmu data dan sedang mengembangkan kemampuan dalam pengolahan data dan machine learning. Mahir dalam menggunakan aplikasi perkantoran seperti Ms. Word, Ms. Excel, dan Ms. Power Point. Berkomitmen untuk terus mengembangkan keterampilan pemrograman dan analisis data.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/fadil-febriansyah-088778212/",
+  github: "https://github.com/fadilfebriansyah",
+  twitter: "https://twitter.com/",
+  instagram: "https://www.instagram.com/fadilfebriyansyah",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position:
+      "Klasifkasi Kualitas Udara Menggunakan Machine Learning:Pembanding Algoritma SVM dan Logistik Regresi.",
+    Type: "Project",
+    Link: "https://github.com/fadilfebriansyah/Klasifikasi-Kualitas-Udara-SVM-dan-Logistik-Regresi",
+  },
+  {
+    Position:
+      "Prediksi Hujan di Australia: Perbandingan Metode Logistic Regression dan SVM untuk Menentukan Kehadiran Hujan pada Hari Berikutnya.",
+    Type: "Project",
+    Link: "https://github.com/fadilfebriansyah/PYTN-KS17-016",
+  },
+  {
+    Position:
+      "Analisis Perbandingan Harga Antar Uber dan Lyft dengan Menggunakan Metode Regresi Linier: Suatu Pendekatan dari Data Boston, MA.",
+    Type: "Project",
+    Link: "https://github.com/fadilfebriansyah/PYTN-KS17-016",
+  },
+  {
+    Position:
+      "Prediksi Keselamatan Pasien: Model Ensemble untuk Menentukan Kematian Akibat Gagal Jantung. ",
+    Type: "Project",
+    Link: "https://github.com/fadilfebriansyah/PYTN-KS17-016",
+  },
+  {
+    Position: "Sertifikat Komputasi Matematika dengan Numpy",
+    Type: "Certificate",
+    Link: "https://drive.google.com/drive/u/1/folders/1wIoyPAZGHFteNddrkb04eGlAp1FQJbRx",
+  },
+  {
+    Position: "Sertifikat Memahami Jupyter Notebook",
+    Type: "Certificate",
+    Link: "https://drive.google.com/drive/u/1/folders/1wIoyPAZGHFteNddrkb04eGlAp1FQJbRx",
+  },
+  {
+    Position: "Sertifikat Mempelajari Machine Learning dengan Python",
+    Type: "Certificate",
+    Link: "https://drive.google.com/drive/u/1/folders/1wIoyPAZGHFteNddrkb04eGlAp1FQJbRx",
+  },
+  {
+    Position: "Sertifikat MSIB",
+    Type: "Certificate",
+    Link: "https://drive.google.com/drive/u/1/folders/1wIoyPAZGHFteNddrkb04eGlAp1FQJbRx",
+  },
+  {
+    Position:
+      "Deteksi Serangan Low Rate DDOS pada Jaringan Tradisional menggunakan Machine Learning dengan Algoritma Decision Tree ",
+    Type: "Publication",
+    Link: "https://www.researchgate.net/publication/373204048_DETEKSI_SERANGAN_LOW_RATE_DDOS_PADA_JARINGAN_TRADISIONAL_MENGGUNAKAN_MACHINE_LEARNING_DENGAN_ALGORITMA_DECISION_TREE",
+  },
+  {
+    Position: "Toko online dengan Laravel dan Vue js",
+    Type: "Publication",
+    Link: "https://play.google.com/store/books/details/Fadil_Febriansyah_TOKO_ONLINE_DENGAN_LARAVEL_DAN_V?id=s8y4EAAAQBAJ&hl=id&gl=US",
+  },
+  {
+    Position: "Membangun RESTful API dengan Go ",
+    Type: "Publication",
+    Link: "https://play.google.com/store/books/details/Fadil_Febriansyah_MEMBANGUN_RESTFUL_API_DENGAN_GO?id=KYXOEAAAQBAJ&hl=id&gl=US",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Frontend Development",
+    Company: "Udemy, YouTube, Google, Medium",
+    Location: "Online",
+    Type: "Full Time",
+    Duration: "Jan 2022 - Present",
+  },
+  {
+    Position: "Bachelor in Electronics & Communication",
+    Company: `Your College Name here`,
+    Location: "Bengaluru",
+    Type: "Full Time",
+    Duration: "Aug 2020 - Present",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+  php: php,
+  python: python,
+  mysql: mysql,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Analisis Sistem Toko Sayur 22",
+    image: projectImage1,
+    description: `Toko Sayuran 22 adalah aplikasi web PHP yang memungkinkan pembelian sayuran dan buah secara online. Didesain untuk mempermudah konsumen berbelanja tanpa harus keluar rumah dan membantu penjual meningkatkan akses pasar.`,
+    techstack: "Project 1",
+  },
+  {
+    title: "Aplikasi Pengelolaan Bengkel Sepede Motor",
+    image: projectImage2,
+    description: `Aplikasi pengelola bengkel adalah aplikasi C# berbasis .NET Core yang dirancang untuk membantu pengelolaan bengkel sepeda motor. Dengan menggunakan pendekatan Model Waterfall, aplikasi ini memudahkan pemilik bengkel dalam melacak dan mengelola perawatan kendaraan secara efisien.`,
+    techstack: "Project 2",
+  },
+  {
+    title: "Aplikasi Toko Online dengan Laravel dan Vue Js",
+    image: projectImage3,
+    description: `
+    Aplikasi toko online menggunakan Laravel, Vue JS, dan MySQL untuk merancang dan mengimplementasikan REST API web service. Laravel bertanggung jawab atas pengelolaan data dan pembuatan endpoint, Vue JS untuk tampilan aplikasi, dan MySQL sebagai basis data. Fitur-fitur utama mencakup tampilan produk, metode pembayaran, dan implementasi web service untuk pengelolaan data. Aplikasi ini memberikan solusi efisien bagi pengembang yang ingin membuat toko online dengan menggunakan Laravel, Vue JS, dan MySQL.`,
+    techstack: "Project 3",
+  },
+  {
+    title:
+      "Klasifkasi Kualitas Udara Menggunakan Machine Learning:Pembanding Algoritma SVM dan Logistik Regresi",
+    image: projectImage4,
+    description: `Proyek ini berfokus pada pengembangan aplikasi klasifikasi kualitas udara di Jakarta. Aplikasi ini memberikan informasi real-time kepada warga Jakarta berdasarkan model klasifikasi yang telah dikembangkan. Evaluasi menunjukkan bahwa model Logistic Regression memiliki akurasi sekitar 83.97%, sedangkan model SVM mencapai akurasi yang lebih tinggi, yaitu sebesar 99.37%. Proyek ini memberikan kontribusi praktis dengan memberikan akses mudah kepada warga Jakarta untuk memantau kualitas udara dengan akurasi tinggi, serta membantu pengambilan keputusan terkait polusi udara di wilayah perkotaan.`,
+    techstack: "Internship 1",
+  },
+  {
+    title:
+      "Prediksi Hujan di Australia: Perbandingan Metode Logistic Regression dan SVM untuk Menentukan Kehadiran Hujan pada Hari Berikutnya",
+    image: projectImage5,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "Studi Independen",
+  },
+  {
+    title:
+      "Analisis Perbandingan Harga Antar Uber dan Lyft dengan Menggunakan Metode Regresi Linier: Suatu Pendekatan dari Data Boston, MA",
+    image: projectImage6,
+    description: `
+    Uber dan Lyft, perusahaan transportasi berbasis aplikasi, telah mengubah cara orang menggunakan layanan transportasi dengan menyediakan alternatif yang nyaman dan terjangkau. Analisis menggunakan Linear Regression pada dataset dari Kaggle untuk mengetahui faktor-faktor yang mempengaruhi harga Uber dan Lyft. Tujuannya adalah membuat model regresi linier yang akurat dan memprediksi harga berdasarkan faktor-faktor tersebut.`,
+    techstack: "Studi Independen",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "fadilfebriansyah76@gmail.com",
+  phone: "0895358349898",
+};
